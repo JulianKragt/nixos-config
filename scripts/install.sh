e7f1ac861b87434caa7478477575d8a1cc8110c3
Julian Kragt <18495180+JulianKragt@users.noreply.github.com>
2024-11-08 21:54:27 +0100

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,37 +1,29 @@
 #!/bin/sh
 
-# partial from Librephoenix
+read -p "Configuration directory (~/.dotfiles): " CONFIG_DIR
+CONFIG_DIR=${CONFIG_DIR:-~/.dotfiles}
+read -p "Set profile (work): " PROFILE
+PROFILE=${PROFILE:-work}
 
-# Automated script to install my dotfiles
-
-# Clone dotfiles
-if [ $# -gt 0 ]
-  then
-    SCRIPT_DIR=$1
-  else
-    SCRIPT_DIR=~/.dotfiles
-fi
-nix-shell -p git --command "git clone https://gitlab.com/JulianKragt/nixos-config $SCRIPT_DIR"
+nix-shell -p git --command "git clone https://github.com/JulianKragt/nixos-config $SCRIPT_DIR"
 
 # Generate hardware config for new system
 sudo nixos-generate-config --show-hardware-config > $SCRIPT_DIR/system/hardware-configuration.nix
 
-# Patch flake.nix with different username/name and remove email by default
+# Auto fill key values
 sed -i "0,/username.*=.*\".*\";/ s/username.*=.*\".*\";/username = \"$(whoami)\";/" $SCRIPT_DIR/control-center.nix
 sed -i "0,/email.*=.*\".*\";/ s/email.*=.*\".*\";/email = \" \";/" $SCRIPT_DIR/control-center.nix
 sed -i "s+~/.dotfiles+$SCRIPT_DIR+g" $SCRIPT_DIR/flake.nix
+sed -i "0,/profile.*=.*\".*\";/ s/profile.*=.*\".*\";/profile = \"$PROFILE\";/" $SCRIPT_DIR/flake.nix
 
 # Open up editor to manually edit flake.nix before install
 if [ -z "$EDITOR" ]; then
     EDITOR=nano;
 fi
-$EDITOR $SCRIPT_DIR/flake.nix;
-
-# Permissions for files that should be owned by root
-sudo $SCRIPT_DIR/harden.sh $SCRIPT_DIR;
+$EDITOR $SCRIPT_DIR/control-center.nix;
 
 # Rebuild system
-sudo nixos-rebuild switch --flake $SCRIPT_DIR#system;
+nix-shell -p git --command "sudo nixos-rebuild switch --flake $SCRIPT_DIR#system"
 
 # Install and build home-manager configuration
-nix run home-manager/master --extra-experimental-features nix-command --extra-experimental-features flakes -- switch --flake $SCRIPT_DIR#user;
+nix run home-manager/master --extra-experimental-features nix-command --extra-experimental-features flakes -- switch --flake $SCRIPT_DIR#user
