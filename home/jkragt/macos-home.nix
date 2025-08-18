ad660688b73df0a254ef40666f24c9b6367a6064
Julian Kragt <jkragt@Julians-MacBook-Pro.local>
2025-08-18 10:14:10 +0200

    add things

--- home/jkragt/macos-home.nix
+++ home/jkragt/macos-home.nix
@@ -9,6 +9,8 @@
     pkgs.tree
     pkgs.neovim
     pkgs.slack
+    pkgs.pnpm
+    pkgs.nodejs
   ];
 
   # Use home.file to create direct links to applications in ~/Applications
@@ -16,6 +18,24 @@
   home.file."Applications/Slack.app" = {
     source = "${pkgs.slack}/Applications/Slack.app";
     recursive = true;
+  };
+  home.file."Applications/KeePassXC.app" = {
+    source = "${pkgs.keepassxc}/Applications/KeePassXC.app";
+    recursive = true;
+  };
+
+  programs.keepassxc = {
+    enable = true;
+    settings = {
+      Browser.Enable = true;
+      GUI = {
+        AdvancedSettings = true;
+        ApplicationTheme = "dark";
+        CompactMode = true;
+        HidePasswords = true;
+      };
+      SSHAgent.Enabled = true;
+    };
   };
 
   home.stateVersion = "25.05";
