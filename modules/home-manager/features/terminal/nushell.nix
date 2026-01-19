f4b7d6e5a452592144e39c6cbdfea3e81fd17edd
/home/jkragt/.config/sops-nix/secrets/git/username </home/jkragt/.config/sops-nix/secrets/git/email>
2026-01-19 21:32:05 +0000

    update

--- modules/home-manager/features/terminal/nushell.nix
+++ modules/home-manager/features/terminal/nushell.nix
@@ -6,13 +6,7 @@
 {
   my.hm.terminal.bash.enable = lib.mkDefault true;
   programs.bash.initExtra = ''
-    # Some programs launch interactive shells and pretend
-    # to use them; such programs always expect a form of POSIX
-    # shell.
-    #
-    # If you don't use programs like that, you can just skip
-    # this conditional.
-    if [ "$TERM" = "xterm-256color" ]; then
+    if [[ -z "$IN_NIX_SHELL" && -z "$NU_VERSION" ]]; then
       exec nu
     fi
   '';
