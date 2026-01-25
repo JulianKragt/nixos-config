5e6e7b92234d5f5c88b9a7edddb409ef782a28c8
Julian Kragt <jkragt@julians-mbp.home>
2026-01-25 23:06:04 +0100

    update

--- modules/home-manager/apps/firefox.nix
+++ modules/home-manager/apps/firefox.nix
@@ -6,58 +6,59 @@
 {
   programs.firefox = {
     enable = true;
-    profiles = {
-      "work" = {
-        name = "work";
-        isDefault = true;
-        extensions.packages = with inputs.firefox-addons.packages.${pkgs.stdenv.hostPlatform.system}; [
-          bitwarden
-          ublock-origin
-        ];
-        settings = {
-          "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
-          "browser.bookmarks.addedImportButton" = false;
-          "browser.toolbars.bookmarks.visibility" = "always";
-        };
-        bookmarks = {
-          force = true;
-          settings = [
-            {
-              toolbar = true;
-              bookmarks = [
-                {
-                  name = "";
-                  url = "localhost:1841";
-                }
-                {
-                  name = "";
-                  url = "localhost:1843";
-                }
-                {
-                  name = "";
-                  url = "https://w3worx.atlassian.net/jira/software/c/projects/APPR/boards/8/backlog?assignee=712020%3Ac6c8b59d-932f-4ea0-ace6-a668b9159412";
-                }
-                {
-                  name = "";
-                  url = "https://bitbucket.org/w3worxdevelopers/appreo/pull-requests/";
-                }
-                {
-                  name = "DB";
-                  url = "https://phpmyadmin.test";
-                }
-                {
-                  name = "Live DB";
-                  url = "https://phpmyadmin.appreo.app:8888/phpmyadmin/index.php?route=/";
-                }
-                {
-                  name = "";
-                  url = "https://www.chatgpt.com";
-                }
-              ];
-            }
-          ];
-        };
-      };
-    };
+    package = pkgs.stable.firefox;
+   profiles = {
+     "work" = {
+       name = "work";
+       isDefault = true;
+       extensions.packages = with inputs.firefox-addons.packages.${pkgs.stdenv.hostPlatform.system}; [
+         bitwarden
+         ublock-origin
+       ];
+       settings = {
+         "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+         "browser.bookmarks.addedImportButton" = false;
+         "browser.toolbars.bookmarks.visibility" = "always";
+       };
+       bookmarks = {
+         force = true;
+         settings = [
+           {
+             toolbar = true;
+             bookmarks = [
+               {
+                 name = "";
+                 url = "localhost:1841";
+               }
+               {
+                 name = "";
+                 url = "localhost:1843";
+               }
+               {
+                 name = "";
+                 url = "https://w3worx.atlassian.net/jira/software/c/projects/APPR/boards/8/backlog?assignee=712020%3Ac6c8b59d-932f-4ea0-ace6-a668b9159412";
+               }
+               {
+                 name = "";
+                 url = "https://bitbucket.org/w3worxdevelopers/appreo/pull-requests/";
+               }
+               {
+                 name = "DB";
+                 url = "https://phpmyadmin.test";
+               }
+               {
+                 name = "Live DB";
+                 url = "https://phpmyadmin.appreo.app:8888/phpmyadmin/index.php?route=/";
+               }
+               {
+                 name = "";
+                 url = "https://www.chatgpt.com";
+               }
+             ];
+           }
+         ];
+       };
+     };
+   };
   };
 }
--- overlays/default.nix
+++ overlays/default.nix
@@ -4,7 +4,7 @@
 }:
 [
   (self: super: {
-    stable = import inputs.nixpkgs-stable { inherit (super) system; config.allowUnfree = true; };
+    stable = import inputs.nixpkgs-stable { system = super.pkgs.stdenv.hostPlatform.system; config.allowUnfree = true; };
     nur = import inputs.nur {
       pkgs = self;
     };
